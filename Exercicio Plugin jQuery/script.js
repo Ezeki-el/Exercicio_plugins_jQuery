38159c9924896255ccc0de94fbd4dba8c49b5307
Ezequiel Oliveira Barboza <132308977+Ezeki-el@users.noreply.github.com>
2023-09-20 10:16:37 -0300

    Add files via upload

--- Exercicio Plugin jQuery/script.js
+++ Exercicio Plugin jQuery/script.js
@@ -18,19 +18,6 @@
             const cpf = $('#cpf').val();
             const endereco = $('#endereco').val();
             const cep = $('#cep').val();
-
-            // Validar CPF (neste exemplo, apenas verificamos se tem 11 dígitos)
-            if (cpf.replace(/\D/g, '').length !== 11) {
-                showError('CPF inválido.');
-                return;
-            }
-
-            // Validar CEP (neste exemplo, apenas verificamos se tem 8 dígitos)
-            if (cep.replace(/\D/g, '').length !== 8) {
-                showError('CEP inválido.');
-                return;
-            }
-
             // Se todos os campos estiverem válidos, você pode enviar o formulário aqui
             // Ou realizar qualquer outra ação necessária
 
