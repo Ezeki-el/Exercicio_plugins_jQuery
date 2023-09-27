ebb8040460362b29d29c48ebff120964bcce69e4
Ezequiel Oliveira Barboza <132308977+Ezeki-el@users.noreply.github.com>
2023-09-26 22:22:30 -0300

    Add files via upload

--- Exercicio Plugin jQuery/script.js
+++ Exercicio Plugin jQuery/script.js
@@ -1,41 +1,7 @@
-$(document).ready(function() {
-    // Aplicar máscaras aos campos
+
+// Aplicar máscaras aos campos
+$(document).ready(function () {
     $('#telefone').mask('(00) 0000-0000');
-    $('#cpf').mask('000.000.000-00');
+    $('#cpf').mask('000.000.000-00', {reverse: true});
     $('#cep').mask('00000-000');
-
-    // Manipular o envio do formulário
-    $('#cadastroForm').submit(function(event) {
-        event.preventDefault();
-
-        // Validar campos
-        const nomeCompleto = $('#nomeCompleto').val();
-        const email = $('#email').val();
-        const telefone = $('#telefone').val();
-        const cpf = $('#cpf').val();
-        const endereco = $('#endereco').val();
-        const cep = $('#cep').val();
-
-        // Validar CPF (neste exemplo, apenas verificamos se tem 11 dígitos)
-        if (cpf.replace(/\D/g, '').length !== 11) {
-            showError('CPF inválido.');
-            return;
-        }
-
-        // Validar CEP (neste exemplo, apenas verificamos se tem 8 dígitos)
-        if (cep.replace(/\D/g, '').length !== 8) {
-            showError('CEP inválido.');
-            return;
-        }
-
-        // Se todos os campos estiverem válidos, você pode enviar o formulário aqui
-        // Ou realizar qualquer outra ação necessária
-
-        // Limpar mensagens de erro
-        $('#errorMessages').empty();
-    });
-
-    function showError(message) {
-        $('#errorMessages').html('<p class="error-message">' + message + '</p>');
-    }
 });
